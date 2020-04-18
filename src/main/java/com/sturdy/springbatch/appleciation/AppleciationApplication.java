21abc69213870ae84d5cda9182e6a9a7bb0957c8
junseongday <junseongday@gmail.com>
2020-04-18 15:59:55 +0900

    frist commit

---
+++ src/main/java/com/sturdy/springbatch/appleciation/AppleciationApplication.java
@@ -0,0 +1,13 @@
+package com.sturdy.springbatch.appleciation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppleciationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppleciationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sturdy/springbatch/appleciation/AppleciationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sturdy.springbatch.appleciation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppleciationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
